b91c7bf435e91ba97694c541188ac976bf04c9ce
Philip Mørch <mrphil2105@gmail.com>
2022-03-26 23:48:10 +0100

    Document types related to screen state

--- Src/MvvmElegance/ScreenStateEventArgs.cs
+++ Src/MvvmElegance/ScreenStateEventArgs.cs
@@ -1,11 +1,21 @@
 namespace MvvmElegance;
 
+/// <summary>
+/// Provides event arguments for when the state of a screen has changed.
+/// </summary>
 public abstract class ScreenStateEventArgs : EventArgs
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScreenStateEventArgs" /> class with the specified previous screen state.
+    /// </summary>
+    /// <param name="previousState">The previous state of the screen.</param>
     protected ScreenStateEventArgs(ScreenState previousState)
     {
         PreviousState = previousState;
     }
 
+    /// <summary>
+    /// Gets the previous state of the screen.
+    /// </summary>
     public ScreenState PreviousState { get; }
 }
