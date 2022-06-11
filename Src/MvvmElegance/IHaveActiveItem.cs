9e3f8bd645450a318bb40f42a8477f4fc74af751
Philip Mørch <mrphil2105@gmail.com>
2022-06-11 02:03:35 +0200

    Document interfaces related to conductors

--- Src/MvvmElegance/IHaveActiveItem.cs
+++ Src/MvvmElegance/IHaveActiveItem.cs
@@ -1,6 +1,13 @@
 namespace MvvmElegance;
 
+/// <summary>
+/// Represents a model that has an active item.
+/// </summary>
+/// <typeparam name="T">The type of item.</typeparam>
 public interface IHaveActiveItem<T>
 {
+    /// <summary>
+    /// Gets or sets the active item.
+    /// </summary>
     T? ActiveItem { get; set; }
 }
--- Src/MvvmElegance/IParent.Generic.cs
+++ Src/MvvmElegance/IParent.Generic.cs
@@ -1,6 +1,14 @@
 namespace MvvmElegance;
 
+/// <summary>
+/// Represents a parent that has one or more children.
+/// </summary>
+/// <typeparam name="T">The type of child.</typeparam>
 public interface IParent<out T> : IParent
 {
+    /// <summary>
+    /// Returns the children of the parent.
+    /// </summary>
+    /// <returns>A collection with the children of the parent or <c>null</c> if there are none.</returns>
     new IEnumerable<T>? GetChildren();
 }
