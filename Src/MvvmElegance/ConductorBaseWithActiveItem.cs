2016f424c6e110a04b5bff7c14f6be8904a09f80
Philip Mørch <mrphil2105@gmail.com>
2022-06-12 19:31:45 +0200

    Document base classes for conductors

--- Src/MvvmElegance/ConductorBaseWithActiveItem.cs
+++ Src/MvvmElegance/ConductorBaseWithActiveItem.cs
@@ -1,16 +1,27 @@
 namespace MvvmElegance;
 
+/// <summary>
+/// Provides the base class for conductors that have an active item.
+/// </summary>
+/// <typeparam name="T">The type of item to conduct.</typeparam>
 public abstract class ConductorBaseWithActiveItem<T> : ConductorBase<T>, IHaveActiveItem<T>
     where T : class
 {
     private T? _activeItem;
 
+    /// <inheritdoc />
     public T? ActiveItem
     {
         get => _activeItem;
         set => ActivateItemAsync(value);
     }
 
+    /// <summary>
+    /// Changes the active item and optionally closes the previous active item.
+    /// </summary>
+    /// <param name="newItem">The item to change the active item to.</param>
+    /// <param name="closePrevious">A boolean indicating whether to close the previous active item.</param>
+    /// <param name="cancellationToken">The cancellation token used to cancel the operation.</param>
     protected virtual async Task ChangeActiveItemAsync(T? newItem, bool closePrevious,
         CancellationToken cancellationToken = default)
     {
@@ -40,16 +51,19 @@ public abstract class ConductorBaseWithActiveItem<T> : ConductorBase<T>, IHaveAc
         RaisePropertyChanged(nameof(ActiveItem));
     }
 
+    /// <inheritdoc />
     protected override Task OnActivateAsync(CancellationToken cancellationToken = default)
     {
         return ScreenExtensions.TryActivateAsync(ActiveItem, cancellationToken);
     }
 
+    /// <inheritdoc />
     protected override Task OnDeactivateAsync(CancellationToken cancellationToken = default)
     {
         return ScreenExtensions.TryDeactivateAsync(ActiveItem, cancellationToken);
     }
 
+    /// <inheritdoc />
     protected override Task OnCloseAsync(CancellationToken cancellationToken = default)
     {
         return this.TryCloseAndCleanUpAsync(ActiveItem, DisposeChildren, cancellationToken);
