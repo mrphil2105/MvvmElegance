b65d0294148bb4b7435f376cf013235f9a37ffb1
Philip Mørch <mrphil2105@gmail.com>
2022-07-07 17:09:36 +0200

    Add unit tests for EventAggregator

---
+++ Tests/MvvmElegance.UnitTests/EventAggregatorTests/PublishTests.cs
@@ -0,0 +1,31 @@
+using Moq;
+
+namespace MvvmElegance.UnitTests.EventAggregatorTests;
+
+public class PublishTests
+{
+    [Theory]
+    [AutoMoqData]
+    public void Publish_RaisesEvent_WhenSubscribed(EventAggregator eventAggregator, Mock<Action<IEvent>> delegateMock,
+        Mock<IEvent> eventMock)
+    {
+        eventAggregator.Subscribe(delegateMock.Object, false);
+
+        eventAggregator.Publish(eventMock.Object);
+
+        delegateMock.Verify(d => d(eventMock.Object), Times.Once);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public void Publish_DoesNotRaiseEvent_WhenUnsubscribed(EventAggregator eventAggregator,
+        Mock<Action<IEvent>> delegateMock, Mock<IEvent> eventMock)
+    {
+        var unsubscriber = eventAggregator.Subscribe(delegateMock.Object, false);
+        unsubscriber.Dispose();
+
+        eventAggregator.Publish(eventMock.Object);
+
+        delegateMock.Verify(d => d(eventMock.Object), Times.Never);
+    }
+}
