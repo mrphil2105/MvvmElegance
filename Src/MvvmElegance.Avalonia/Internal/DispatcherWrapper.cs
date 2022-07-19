5de7e17fbb39b240fa5b5ca69cc2a12e22d755df
Philip Mørch <mrphil2105@gmail.com>
2022-07-19 19:34:45 +0200

    Implement wrapper for Avalonia dispatcher

---
+++ Src/MvvmElegance.Avalonia/Internal/DispatcherWrapper.cs
@@ -0,0 +1,33 @@
+using Avalonia.Threading;
+
+namespace MvvmElegance.Internal;
+
+internal class DispatcherWrapper : IDispatcher
+{
+    private readonly Dispatcher _dispatcher;
+
+    public DispatcherWrapper(Dispatcher dispatcher)
+    {
+        _dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
+    }
+
+    public bool IsCurrent => _dispatcher.CheckAccess();
+
+    public void Post(Action action)
+    {
+        _dispatcher.Post(action, DispatcherPriority.Send);
+    }
+
+    public void Send(Action action)
+    {
+        if (IsCurrent)
+        {
+            action();
+        }
+        else
+        {
+            _dispatcher.InvokeAsync(action, DispatcherPriority.Send)
+                .Wait();
+        }
+    }
+}
