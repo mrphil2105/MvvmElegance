56d3fdfc06bf4a5cfdbbd8a6e5fa82b0324decd2
Philip Mørch <mrphil2105@gmail.com>
2022-08-08 20:45:04 +0200

    Make internals visible to DependencyInjection project

--- Src/MvvmElegance.Avalonia/Properties/AssemblyInfo.cs
+++ Src/MvvmElegance.Avalonia/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Runtime.CompilerServices;
 using Avalonia.Metadata;
 
 [assembly: InternalsVisibleTo("MvvmElegance.Avalonia.Autofac")]
+[assembly: InternalsVisibleTo("MvvmElegance.Avalonia.DependencyInjection")]
 [assembly: XmlnsDefinition("https://github.com/mrphil2105/MvvmElegance", "MvvmElegance.Xaml")]
--- Src/MvvmElegance/Properties/AssemblyInfo.cs
+++ Src/MvvmElegance/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MvvmElegance.Avalonia")]
 [assembly: InternalsVisibleTo("MvvmElegance.Avalonia.Autofac")]
+[assembly: InternalsVisibleTo("MvvmElegance.Avalonia.DependencyInjection")]
