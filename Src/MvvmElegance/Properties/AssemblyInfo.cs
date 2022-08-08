edc8ad69dcaaa6dcdc37659f347013e02dab8203
Philip Mørch <mrphil2105@gmail.com>
2022-08-08 22:51:42 +0200

    Remove internals visibility to IoC container projects

--- Src/MvvmElegance/Properties/AssemblyInfo.cs
+++ Src/MvvmElegance/Properties/AssemblyInfo.cs
@@ -1,5 +1,3 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MvvmElegance.Avalonia")]
-[assembly: InternalsVisibleTo("MvvmElegance.Avalonia.Autofac")]
-[assembly: InternalsVisibleTo("MvvmElegance.Avalonia.DependencyInjection")]
